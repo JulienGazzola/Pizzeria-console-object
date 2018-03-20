bf58ebdeaf66419cd4b1b370448bb9c65ad4a343
Julien.gazzola <julieng.gazzola@gmail.com>
2018-03-20 14:52:45 +0100

    adding option 1 + 2

--- src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,13 +1,26 @@
 package fr.pizzeria.console;
 
 import fr.pizzeria.model.*;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 	PizzeriaAdminConsoleApp {
 	
 	public static void main(String[] args) {
+		/*Pizza pépé = new Pizza(0, "PEP", "Pépérono", 12.50);*/
+		Pizza[] arrayPizza = {
+				new Pizza(0, "PEP", "Pépérono", 12.50),
+				new Pizza(1, "MAR", "Margherita", 14.00),
+				new Pizza(2, "REIN", "La Reine", 11.50),
+				new Pizza(3, "FRO", "La 4 fromages", 12.00),
+				new Pizza(4, "CAN", "La cannibale", 12.50),
+				new Pizza(5, "SAV", "La savoyarde", 13.00),
+				new Pizza(6, "ORI", "L'orientale", 13.50),
+				new Pizza(7, "IND", "L'indienne", 14.00)
+		};
 		
-		
+		int tmp = 8;
 		int nbChoice = 0;
 		while(nbChoice != 99){
 			System.out.println("** Pizzeria Administration\n"
@@ -21,9 +34,35 @@ public class 	PizzeriaAdminConsoleApp {
 			switch (nbChoice){
 			case 1: 
 				System.out.println("Liste des pizzas");
+				for (int i = 0; i < arrayPizza.length ;i++){
+					System.out.println(arrayPizza[i].getCode() + " -> " 
+				+ arrayPizza[i].getLibelle() + " ("
+				+ arrayPizza[i].getPrix() + " €)");
+				}
 				break;
 			case 2:
-				System.out.println("Ajout d'une nouvelle pizza");
+				System.out.println("Ajout d'une nouvelle pizza\n"
+						+ "Veuillez saisir le code : ");
+				Scanner newCode = new Scanner(System.in);
+				String nbCode = newCode.next();
+				
+				System.out.println("Veuillez saisir le nom (sans espace) : ");
+				Scanner newNom = new Scanner(System.in);
+				String nbNom = newNom.next();
+				
+				System.out.println("Veuillez saisir le prix : ");
+				Scanner newPrix = new Scanner(System.in);
+				double nbPrix = newPrix.nextDouble();
+				
+				if (tmp < arrayPizza.length){
+					arrayPizza[tmp] = new Pizza(nbCode, nbNom, nbPrix);
+				}
+				else {
+					arrayPizza = Arrays.copyOf(arrayPizza, arrayPizza.length + 1);
+					arrayPizza[tmp] = arrayPizza[tmp] = new Pizza(nbCode, nbNom, nbPrix);
+				}
+				tmp++;
+				
 				break;
 			case 3:
 				System.out.println("Mise à jour d'une pizza");
--- src/main/java/fr/pizzeria/model/Pizza.java
+++ src/main/java/fr/pizzeria/model/Pizza.java
@@ -1,10 +1,10 @@
 package fr.pizzeria.model;
 
 public class		Pizza {
-	int 	id;
-	String	code;
-	String 	libelle;
-	double	prix;
+	private int 	id;
+	private String	code;
+	private String 	libelle;
+	private double	prix;
 	
 	public			Pizza(String code, String libelle, double prix){
 		this.code = code;
@@ -15,6 +15,31 @@ public class		Pizza {
 		this.id = id;
 		this.code = code;
 		this.libelle = libelle;
+		this.prix = prix;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
 		this.prix = prix;
 	}
 }
